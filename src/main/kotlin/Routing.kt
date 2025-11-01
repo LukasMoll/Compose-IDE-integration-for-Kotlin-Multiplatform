184f23b7f1eb91cfc34f0a73687eb7a3cb9a74d5
Lukas Molleman <Lukas.Molleman@gmail.com>
2025-11-01 11:10:26 +0100

    Added simple frontend

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,14 +2,17 @@ package com.main
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/", "")
+
         get("/") {
-            call.respondText("Hello World!")
+            call.respondRedirect("/index.html")
         }
         post("/validate-code") {
             call.receive<String>()
